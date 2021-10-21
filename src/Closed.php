41d31688d65d0d8d72463a911144e89c9fb9269b
rve <r.verhoeven@weat.de>
2021-10-21 11:53:32 +0200

    Cosmetics

--- src/Closed.php
+++ src/Closed.php
@@ -5,5 +5,6 @@ namespace alcamo\exception;
 /// Exception thrown by an attempt to use an already closed object
 class Closed extends AbstractObjectStateException
 {
-    public const NORMALIZED_MESSAGE = 'Attempt to use closed {objectType} {object}';
+    public const NORMALIZED_MESSAGE =
+        'Attempt to use closed {objectType} {object}';
 }
